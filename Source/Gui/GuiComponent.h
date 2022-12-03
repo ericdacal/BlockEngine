0c9f16aee9f560328e22a249ac106c29800bb913
Eric <eric@dacal.es>
2022-12-04 00:17:41 +0100

    Implementation of base component to Gui components

--- Source/Gui/GuiComponent.h
+++ Source/Gui/GuiComponent.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "Globals.h"
-#include "imgui.h"
-#include "imgui_impl_sdl.h"
-#include "imgui_impl_opengl3.h"
+#include "../Globals.h"
+#include <imgui.h>
+#include <imgui_impl_sdl.h>
+#include <imgui_impl_opengl3.h>
 
 class Application;
 
