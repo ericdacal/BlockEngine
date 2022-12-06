5a92cb0482c226149d8382f58dc8e3e886ade579
Eric <eric@dacal.es>
2022-12-06 18:01:20 +0100

    Implementation of a new module to draw objects

---
+++ Source/Modules/ModuleDrawer.cpp
@@ -0,0 +1,45 @@
+#include "ModuleDrawer.h"
+#include "../Application.h"
+#include "ModuleWindow.h"
+#include "SDL.h"
+
+
+
+ModuleDrawer::ModuleDrawer()
+{
+}
+
+// Destructor
+ModuleDrawer::~ModuleDrawer()
+{
+}
+
+bool ModuleDrawer::Init() {
+	bool ret = true;
+	
+	m = Model();
+	m.Load("../Source/resources/BakerHouse.fbx");
+	return true;
+}
+
+update_status ModuleDrawer::PreUpdate()
+{
+
+	update_status ret = UPDATE_CONTINUE;
+	
+	return ret;
+}
+
+
+update_status ModuleDrawer::Update()
+{
+	update_status ret = UPDATE_CONTINUE;
+	m.Draw();
+
+	return ret;
+}
+
+void ModuleDrawer::changeModel(const char* path) {
+	m = Model();
+	m.Load(path);
+}
